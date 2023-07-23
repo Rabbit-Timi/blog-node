0f1ce5f58d4c1ab9ffece3aa90fa570a43f2731e
timmtiy <timmtiy@163.com>
2023-07-23 10:16:31 +0800

    feat: config

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,6 @@
 // 资源根地址
-exports.BASE_PATH = '/Users/zhangyu/Downloads/public'
+// exports.BASE_PATH = '/Users/zhangyu/Downloads/public'
+exports.BASE_PATH = '/root/public'
 
 // 服务器地址
-exports.BASEURL = 'http://192.168.31.135:80'
+exports.BASEURL = 'http://119.45.167.11:80'
