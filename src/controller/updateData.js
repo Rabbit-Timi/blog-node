c4b6638df2d50eb2c863ab3d1e2e3175295674d0
timmtiy <timmtiy@163.com>
2023-07-27 14:23:48 +0800

    fix: test webHook

--- src/controller/updateData.js
+++ src/controller/updateData.js
@@ -19,24 +19,24 @@ exports.updateDirectoryTree = async (req, res) => {
 
 // 更新文章
 exports.webHookPapersFile = async (req, res) => {
-  console.log('webHookPapersFile',req.body) //一大串仓库push的信息，用它来计算动态签名
-  const sign = req.headers['x-hub-signature']   //输出为：sha1=${secret的加密字符串}
-  const event = req.headers['x-github-event']    //输出为：事件名称(push)
-  // const commitID = req.headers['x-github-delivery'] //输出为：commitID
-  if(event=='push'){
-    // 根据请求的body和secret计算sha1的值
-    const hmac = crypto.createHmac('sha1','bilibili123456');
-    hmac.update(new Buffer(JSON.stringify(req.body))); //req.body时github传过来的post数据(跟request.body一样的)
-    const signature = 'sha1=' + hmac.digest('hex'); //用这个跟sign对比
-    // 可在此验证sign真伪
-    if(signature == sign){
-      let cwd = process.cwd()
-      runCmd('sh', [path.join(cwd,'scripts/pullClover.sh')], function(res){
-        console.log(res) //res返回的是shell命令操作后在命令行终端显示的字符串，这里是一些git操作的提示
-        saveDirectoryTree()
-      });
-    }
-  }
+  console.log('webHookPapersFile',req) //一大串仓库push的信息，用它来计算动态签名
+  // const sign = req.headers['x-hub-signature']   //输出为：sha1=${secret的加密字符串}
+  // const event = req.headers['x-github-event']    //输出为：事件名称(push)
+  // // const commitID = req.headers['x-github-delivery'] //输出为：commitID
+  // if(event=='push'){
+  //   // 根据请求的body和secret计算sha1的值
+  //   const hmac = crypto.createHmac('sha1','bilibili123456');
+  //   hmac.update(new Buffer(JSON.stringify(req.body))); //req.body时github传过来的post数据(跟request.body一样的)
+  //   const signature = 'sha1=' + hmac.digest('hex'); //用这个跟sign对比
+  //   // 可在此验证sign真伪
+  //   if(signature == sign){
+  //     let cwd = process.cwd()
+  //     runCmd('sh', [path.join(cwd,'scripts/pullClover.sh')], function(res){
+  //       console.log(res) //res返回的是shell命令操作后在命令行终端显示的字符串，这里是一些git操作的提示
+  //       saveDirectoryTree()
+  //     });
+  //   }
+  // }
   res.send({
     errno: 0,
     msg: 'hello world'
