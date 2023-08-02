1951ecafe65daafb8c4d83e5a54ed9d8882a5a00
timmtiy <timmtiy@163.com>
2023-08-02 12:31:04 +0800

    fix: 隐藏地址

--- src/app/config.js
+++ src/app/config.js
@@ -1,13 +1,9 @@
 // 资源根地址
-// exports.BASE_PATH = '/Users/zhangyu/Downloads/public'
 exports.BASE_PATH = '/home/ubuntu/public'
 
 // 服务器地址
-exports.BASEURL = 'http://119.45.167.11:8888'
+exports.BASEURL = ''
 // exports.BASEURL = 'http://192.168.31.135:8888'
-
-// 文章git地址
-// exports.BLOG_PAPERS_REPOSITORY_NAME = 'blog-papers'
 
 // git webhook token
 exports.GIT_WEBHOOK_TOKEN = '318a1115ba5b1bb46ec4f984b4db6ec152c6b0fe'
